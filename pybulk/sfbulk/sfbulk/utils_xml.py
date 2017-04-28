e486f8a55cdcbbc561beaf29cb819f7842512e4f
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2017-04-28 10:45:51 -0500

    fix xml parser, to support duplicate keys in dictinonary

--- pybulk/sfbulk/sfbulk/utils_xml.py
+++ pybulk/sfbulk/sfbulk/utils_xml.py
@@ -52,9 +52,20 @@ def parseXMLResult(raw_xml):
     parse_resp = xml.dom.minidom.parseString(raw_xml)
     Root = parse_resp.documentElement
 
+    # items having same key are supported here
     for child in Root.childNodes:
         if child.nodeType == ELEMENT_NODE:
-            retval = _parseElement(child.childNodes, retval)
+            keyval = {}
+            keyval = _parseElement(child.childNodes, keyval)
+            if keyval:
+                key = keyval.keys()[0]
+                if not retval:
+                    retval = keyval
+                elif key in retval.keys():
+                    duplicate_key = '%s-%d' % (key, len(retval) + 1)
+                    retval[duplicate_key] = keyval[key]
+                else:
+                    retval.update(keyval)
     return retval
 
 
